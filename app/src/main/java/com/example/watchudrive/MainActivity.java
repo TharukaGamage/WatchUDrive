2a7f7900f2acf246d89052cb6ebe2acedf842d5e
TharukaGamage <tharuka.kg@gmail.com>
2019-08-18 06:59:02 +0530

    Register interface done

--- app/src/main/java/com/example/watchudrive/MainActivity.java
+++ app/src/main/java/com/example/watchudrive/MainActivity.java
@@ -2,14 +2,34 @@ package com.example.watchudrive;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnRegister;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);//
-        //second commit
+        setUIPropaties();
+
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intentRegister =  new Intent(MainActivity.this,RegisterActivity.class);
+                 MainActivity.this.startActivity(intentRegister);
+            }
+        });
     }
+
+    public void setUIPropaties()
+    {
+        btnRegister = (Button)findViewById(R.id.btnRegister);
+    }
+
 }
---
+++ app/src/main/java/com/example/watchudrive/RegisterActivity.java
@@ -0,0 +1,19 @@
+package com.example.watchudrive;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    Button btnRegister;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+        btnRegister = (Button)findViewById(R.id.btnRegister);
+    }
+}
